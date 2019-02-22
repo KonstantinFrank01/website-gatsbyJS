159a3b5bb32b7a5afcf238bedf90bc29e77d8dbf
KonstantinFrank01 <k.frank@students.htl-leonding.ac.at>
2019-02-22 12:45:41 +0100

    Color Box erstellt

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -10,16 +10,19 @@ export default () => (
     <div style={{backgroundColor: 'white'}}>
         <NavigationBar />
         <img src={heroImage} alt="heroImage" style={{position: 'absolute', top: '10%'}}/> 
-        <div style={{position: 'absolute', left: '7.5%', top: '100%'}}>
-            <DepBox1></DepBox1>
-        </div>
-        <div style={{position: 'absolute', left: '37.5%', top: '100%'}}>
-            <DepBox2></DepBox2>
-        </div>
-        <div style={{position: 'absolute', left: '67.5%', top: '100%'}}>
-            <DepBox3></DepBox3>
+            <div style={{position: 'absolute', left: '7.5%', top: '100%'}}>
+                <DepBox1></DepBox1>
+            </div>
+            <div style={{position: 'absolute', left: '37.5%', top: '100%'}}>
+                <DepBox2></DepBox2>
+            </div>
+            <div style={{position: 'absolute', left: '67.5%', top: '100%'}}>
+                <DepBox3></DepBox3>
+            </div>
+        
+        <div id="colorBox1" style={{ backgroundColor: '#ffe5e0', width: '100%', height: '50%', position: 'absolute', top: '140%', zIndex: '1000'}}>    
+            <p style={{position: 'absolute'}}>next content</p>
         </div>
         
-        <p style={{position: 'absolute', top: '150%'}}>next content</p>
     </div>
 )
