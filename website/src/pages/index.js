7f4dc538c80a245e63741cf9c7c4885d9121ce14
KonstantinFrank01 <k.frank@students.htl-leonding.ac.at>
2019-02-25 13:48:27 +0100

    Versuch TypeEffect - funktioniert noch nicht

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -1,5 +1,7 @@
 import React from "react"
 import NavigationBar from "../components/navigationBar"
+import ITyped from "../utils/ityped-master/src/index"
+/*import Typing from "../utils/typing"*/
 import DepBox1 from "../components/depBox1"
 import DepBox2 from "../components/depBox2"
 import DepBox3 from "../components/depBox3"
@@ -21,7 +23,23 @@ export default () => (
         <div style={{position: 'absolute', left: '67.5%', top: '100%'}}>
             <DepBox3></DepBox3>
         </div>
-        <div id="colorBox2" style={{ backgroundColor: '#ffe5e0', width: '100%', height: '50%', position: 'absolute', top: '240%', zIndex: '1000'}}></div>
-            
+        <hr id="headingLine" style={{ zIndex: '1000', position: 'absolute', top: '155%', left: '7.5%', height: '0.7%', width: '7.5%', border: 'none', color: '#262222', backgroundColor: '#262222'}}/>
+        <span style={{ position: 'absolute', top: '154%', left: '17%'}}>DIGITAL</span>
+        <span id="element"style={{ position: 'absolute', top: '154%', left: '27%'}}></span>
+
+        <div id="colorBox2" style={{ backgroundColor: '#ffe5e0', width: '100%', height: '50%', position: 'absolute', top: '240%', zIndex: '1000'}}></div> 
     </div>
 )
+
+/*var options = {
+    strings: ['Creators', 'Big-Thinkers'],
+    typeSpeed: 55,
+    backSpeed: 30,
+    startDelay: 500,
+    backDelay: 500,
+    loop: false,
+    showCursor: true,
+    cursorChar: "|",
+    onFinished: function(){}
+}
+ITyped.ityped.init('#element', options);*/
---
+++ website/src/utils/typing.js
@@ -0,0 +1,14 @@
+
+
+var options = {
+    strings: ['Creators', 'Big-Thinkers'],
+    typeSpeed: 55,
+    backSpeed: 30,
+    startDelay: 500,
+    backDelay: 500,
+    loop: false,
+    showCursor: true,
+    cursorChar: "|",
+    onFinished: function(){}
+}
+ityped.init('#element', options);
