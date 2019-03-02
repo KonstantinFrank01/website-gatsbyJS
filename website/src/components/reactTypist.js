3e32ef65fd10f9efb53182b8b37773851c027cd7
KonstantinFrank01 <k.frank@students.htl-leonding.ac.at>
2019-03-02 15:49:41 +0100

    Text eingefügt

--- website/src/components/reactTypist.js
+++ website/src/components/reactTypist.js
@@ -2,11 +2,11 @@ import React from 'react';
 import Typist from 'react-typist';
  
 export default ({children}) => (
-    <Typist>
-        <span className="my-custom-class" style={{ position: 'absolute', top: '150.5%', left: '32%', color: '#ffb246', fontSize: '3rem', fontWeight: 'bold'}}>CREATORS .</span><br />
-        <Typist.Backspace count={10} delay={200} />
-        <span style={{ position: 'absolute', top: '150.5%', left: '32%', color: '#ffb246', fontSize: '3rem', fontWeight: 'bold'}}>EXPERTS .</span>
-        <Typist.Backspace count={9} delay={200} />
-        <span style={{ position: 'absolute', top: '150.5%', left: '32%', color: '#ffb246', fontSize: '3rem', fontWeight: 'bold'}}>BIG-THINKERS .</span>
+    <Typist avgTypingDelay={110}>
+        <span className="my-custom-class" style={{ position: 'absolute', top: '145.5%', left: '32%', color: '#ffb246', fontSize: '3rem'}}>CREATORS .</span><br />
+        <Typist.Backspace count={10} delay={400}/>
+        <span style={{ position: 'absolute', top: '145.5%', left: '32%', color: '#ffb246', fontSize: '3rem'}}>BIG-THINKERS .</span>
+        <Typist.Backspace count={14} delay={400} />
+        <span style={{ position: 'absolute', top: '145.5%', left: '32%', color: '#ffb246', fontSize: '3rem'}}>EXPERTS .</span>
     </Typist>
 )
--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -12,10 +12,10 @@ import heroImage from "../images/heroImage3.jpeg"
 export default () => (
     <div style={{backgroundColor: 'white'}}>
         <NavigationBar />
-        <img src={Logo} alt="Firmenlogo" style={{position: 'absolute', top: '1.5%', left: '1.4%', height: '53px', width: '215px'}} /> 
+        <img src={Logo} alt="Firmenlogo" style={{position: 'absolute', top: '1.5%', left: '3%', height: '53px', width: '215px'}} /> 
         <img src={heroImage} alt="heroImage" style={{position: 'absolute', top: '10%'}}/> 
             
-        <div id="colorBox1" style={{ backgroundColor: '#F2F2F2', width: '100%', height: '115%', position: 'absolute', top: '91%'}}></div>
+        <div id="colorBox1" style={{ backgroundColor: '#F2F2F2', width: '100%', height: '115.5%', position: 'absolute', top: '91%'}}></div>
         <div style={{position: 'absolute', left: '7.5%', top: '100%'}}>
             <DepBox1></DepBox1>
         </div>
@@ -25,10 +25,18 @@ export default () => (
         <div style={{position: 'absolute', left: '67.5%', top: '100%'}}>
             <DepBox3></DepBox3>
         </div>
-        <hr id="headingLine" style={{ zIndex: '1000', position: 'absolute', top: '155%', left: '7.5%', height: '0.7%', width: '7.5%', border: 'none', color: '#ffb246', backgroundColor: '#ffb246'}}/>
-        <span style={{ position: 'absolute', top: '150.5%', left: '17.4%', color: '#ffb246', fontSize: '3rem', fontWeight: 'bold'}}>DIGITAL</span>
+        <hr id="headingLine" style={{ zIndex: '1000', position: 'absolute', top: '150%', left: '7.5%', height: '0.7%', width: '7.5%', border: 'none', color: '#ffb246', backgroundColor: '#ffb246'}}/>
+        <span style={{ position: 'absolute', top: '145.5%', left: '17.4%', color: '#ffb246', fontSize: '3rem'}}>DIGITAL</span>
         <Typist></Typist>
+        <span style={{ position: 'absolute', top: '157%', left: '17.4%', color: '#7b9fff', fontSize: '2.3rem'}}>
+            A&amp;F Agency is a full service digital agency for <br />
+            purpose-driven organizations, visionaries, and <br />
+            big-thinkers. We’re a tight knit team of designers, <br />
+            strategists, technologists, project managers, <br />
+            planners, and practiced problem-solvers ready to <br />
+            help you take your ideas from concept to reality.
+        </span>
 
-        <div id="colorBox2" style={{ backgroundColor: '#ffe5e0', width: '100%', height: '50%', position: 'absolute', top: '240%', zIndex: '1000'}}></div> 
+        <div id="colorBox2" style={{ backgroundColor: '#fffdd0', width: '100%', height: '50%', position: 'absolute', top: '206.5%', zIndex: '1000'}}></div> 
     </div>
 )
